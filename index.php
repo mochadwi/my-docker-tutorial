458900097410f55dbf3989f718c7ea7c7c9e0c16
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2016-05-20 20:12:39 +0700

    [NEW] add PHP Container, and modify the Nginx container to build from the official one.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello Docker!</title>
+    </head>
+    <body>
+        <img src="http://blog.osteel.me/images/2015/12/18/docker-tutorial2.gif"
+        alt="Hello Docker!">
+    </body>
+</html>
